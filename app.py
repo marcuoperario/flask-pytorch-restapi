259397072f5a2e8b2586b9202b85aa4c7aa5d306
Marcu Operario <marcuoperario@gmail.com>
2023-04-30 15:15:00 +0800

    feat: add '_validate_not_a_forked_repo' before loading model

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ firebase_config = {
   "serviceAccount": "serviceAccount.json"
 }
 
+torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
 model = torch.hub.load("ultralytics/yolov5", "custom", path = "/opt/render/project/src/ML/weights/best.pt", trust_repo=True)
 firebase = pyrebase.initialize_app(firebase_config)
 auth = firebase.auth()
