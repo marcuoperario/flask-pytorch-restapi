8923c7c21a9a364753f65c64fc1008320a48288a
Marcu Operario <marcuoperario@gmail.com>
2023-04-30 11:52:36 +0800

    feat: add trust_repo property

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ firebase_config = {
   "serviceAccount": "serviceAccount.json"
 }
 
-model = torch.hub.load("ultralytics/yolov5", "custom", path = "ML/weights/best.pt")
+model = torch.hub.load("ultralytics/yolov5", "custom", path = "ML/weights/best.pt", trust_repo=True)
 firebase = pyrebase.initialize_app(firebase_config)
 auth = firebase.auth()
 db = firebase.database()
